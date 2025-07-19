f3c96dc262108a745687d2d9dd0fda36d20240d5
observer04 <tripathysarojinee@gmail.com>
2025-07-20 01:03:38 +0530

    parse lines from request and look for user-agent. then split on : , once and done. check for endpoint and respond

--- app/main.py
+++ app/main.py
@@ -14,8 +14,16 @@ def main():
         c_socket, ret_addr= server_socket.accept() # wait for client
         #read request from socket , parse it and respond
         request = c_socket.recv(1024).decode()      #read first part of encoded http request
-        request_line = request.splitlines()[0]
-        method, path, _ = request_line.split()
+        request_lines = request.splitlines()
+        first_request_line = request_lines[0]
+        method, path, _ = first_request_line.split()
+        
+        user_agent = ""
+        #extract User-Agent Header:
+        for line in request_lines[1:]:
+            if line.lower().startswith('user-agent'):
+                user_agent= line.split(":", maxsplit=1)[1].strip()
+                break
         
         
         if path == "/":
@@ -33,7 +41,14 @@ def main():
                 "\r\n"
                 f"{echo_value}"
             )
-        
+        elif path == '/user-agent':
+            response = (
+                "HTTP/1.1 200 OK\r\n"
+                "Content-Type: text/plain\r\n"
+                f"Content-Length: {len(user_agent)}\r\n"
+                "\r\n"
+                f"{user_agent}"
+            )
         
         
         else:
